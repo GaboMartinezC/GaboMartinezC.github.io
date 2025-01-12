388e81b1d3c29193e58fa64a54202b5674966fbb
Gabriel de Jesús Martínez Camareno <martinezcgabriel24@gmail.com>
2025-01-11 22:42:28 -0600

    Update index.js

--- Portfolio/assets/index.js
+++ Portfolio/assets/index.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+    $('.contenido').hide();
+    $('#inicio').show();
+    $(".esText").hide();
+    $('#eglTraduc').click(function () {
+        $(".esText").hide();
+        $(".egText").show();
+    });
+    $('#esTraduc').click(function () {
+        $(".esText").show();
+        $(".egText").hide();
+    });
 });
